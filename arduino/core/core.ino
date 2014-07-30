abd906c7729eee0e02685a44e166d465b18eddd4
sass3r <mrsblast@gmail.com>
2014-07-30 11:45:24 -0400

    actualizado firmware core.ino

--- arduino/core/core.ino
+++ arduino/core/core.ino
@@ -9,6 +9,7 @@
 #define M7 4
 #define M8 5
 
+
 #define ANG_M1 87
 #define ANG_M2 86
 #define ANG_M3 85
@@ -17,12 +18,13 @@
 #define ANG_M5 87
 #define ANG_M6 87
 
-#define ANG_M7 180
+#define ANG_M7 120
 #define ANG_M8 90
-
 #define LI 0
 #define LS 180
 
+#define CAM1_RIGHT_FWD '\xf5'
+#define CAM1_LEFT_BWD '\xf6'
 #define CAM1_LEFT '\xf7' 
 #define CAM1_RIGHT '\xf8'
 #define CAM2_UP '\xf9'
@@ -38,26 +40,16 @@ char cmd;
 
 void setup(){
   
-  motor1.attach(M1);
-  motor2.attach(M2);
-  motor3.attach(M3);
-  motor4.attach(M4);
-  motor5.attach(M5);
-  motor6.attach(M6);
+  atachar();
+  ini();
+  detachar();
   
-  cam1.attach(M7);
-  cam2.attach(M8);
   
-  motor1.write(ANG_M1);
-  motor2.write(ANG_M2);
-  motor3.write(ANG_M3);
-  motor4.write(ANG_M4);
-  motor5.write(ANG_M5);
-  motor6.write(ANG_M6);
-  
+  camatach();
   cam1.write(ANG_M7);
   cam2.write(ANG_M8);
-  
+  delay(1000);
+  camdetach();
   Serial.begin(9600);
  
 }
@@ -67,32 +59,23 @@ void loop(){
     cmd = (char) Serial.read();
     if(cmd == ADELANTE){
       Serial.println("adelante");
-      avanzar();
-      delay(2000);
-      parar();
+      avanzar();      
     }else{
       if(cmd == RETROCEDER){
         Serial.println("retroceder");
         retroceder();
-        delay(2000);
-        parar();  
       }else{
         if(cmd == IZQUIERDA){
           Serial.println("izquierda");
           izq();
-          parar();
-        
         }else{
           if(cmd == DERECHA){
             Serial.println("derecha");
             der();
-            parar();
-          
           }else{
             if(cmd == PARAR){
               Serial.println("parar");
               parar();
-            
             }else{
               if(cmd == CAM1_LEFT){
                 Serial.println("moviendo camara hacia la izq");
@@ -108,18 +91,62 @@ void loop(){
                   }else{
                     if(cmd == CAM2_DOWN){
                       Serial.println("moviendo camara hacia abajo");
-                      cam2down();
-                    
+                      cam2down();                    
+                    }else{
+                      if(cmd == CAM1_LEFT_BWD){
+                        Serial.println("moviendo camara hacia la izq BWD");
+                        cam1izqbwd();
+                      }else{
+                        if(cmd == CAM1_RIGHT_FWD){
+                          Serial.println("moviendo camara hacia la der FWD");
+                          cam1derfwd();
+                        }
+                      }
                     }
                   }
                 }
-              }
+              }  
             }
           }
         }
       }
     }
-  }  
+  } 
+  Serial.flush(); 
+}
+
+
+void detachar(){
+  motor1.detach();
+  motor2.detach();
+  motor3.detach();
+  motor4.detach();
+  motor5.detach();
+  motor6.detach();
+    
+}
+
+void atachar(){
+  motor1.attach(M1);
+  motor2.attach(M2);
+  motor3.attach(M3);
+  motor4.attach(M4);
+  motor5.attach(M5);
+  motor6.attach(M6);
+    
+}
+
+
+void camatach(){
+    cam1.attach(M7);
+    cam2.attach(M8);
+
+}
+
+void camdetach(){
+    cam1.detach();
+    cam2.detach();
+
 }
 
 
@@ -140,7 +167,135 @@ void atras(Servo motor, int li){
 
 }
 
+
 void parar(){
+  ini();    
+  detachar();
+}
+
+void izq(){
+  atachar();
+  adelante(motor1,0);
+  adelante(motor3,0);
+  adelante(motor5,0);
+  atras(motor2,1);
+  atras(motor4,1);
+  atras(motor6,1);  
+  delay(2000);
+  detachar();
+}
+
+void der(){
+ atachar();
+ adelante(motor2,1);
+ adelante(motor4,1);
+ adelante(motor6,1);
+ atras(motor1,0);
+ atras(motor3,0);
+ atras(motor5,0);
+ delay(2000);
+ detachar();
+}
+
+void avanzar(){
+  atachar();
+  adelante(motor1,0);
+  adelante(motor2,1);
+  adelante(motor3,0);
+  adelante(motor4,1);
+  adelante(motor5,0);
+  adelante(motor6,1);
+  delay(2000);
+  detachar();
+}
+
+void retroceder(){
+  atachar();
+  atras(motor1,0);
+  atras(motor3,0);
+  atras(motor5,0);
+  atras(motor2,1);
+  atras(motor4,1);
+  atras(motor6,1);
+  delay(2000);
+  detachar();
+}
+
+void cam1izq(){
+  camatach();
+  int ang = cam1.read() - 10;
+  if(ang < LS && ang > LI){
+    cam1.write(ang);
+  }
+  delay(100);
+  camdetach();
+}
+
+void cam1izqbwd(){
+  int nro = 0;
+  camatach();
+  Serial.println(cam1.read());
+  int ang = cam1.read() - 35;
+  if(ang < LS && ang > LI){
+    while(nro < 7){
+      cam1.write(cam1.read()-5);
+      delay(70);
+      nro = nro + 1;
+    }
+  }
+  Serial.println(cam1.read());
+  camdetach();
+
+}
+
+void cam1derfwd(){
+  int nro = 0;
+  camatach();
+  Serial.println(cam1.read());
+  int ang = cam1.read() + 35;
+  if(ang < LS && ang > LI){
+    while(nro < 7){
+      cam1.write(cam1.read()+5);
+      delay(70);
+      nro = nro + 1;
+    }
+  }
+  Serial.println(cam1.read());
+  camdetach();
+}
+
+void cam1der(){
+  camatach();
+  int ang = cam1.read() + 10;
+  if(ang < LS && ang > LI){
+    cam1.write(ang);
+  }
+  delay(100);
+  camdetach();
+
+}
+
+void cam2up(){
+  camatach();
+  int ang = cam2.read() + 10;
+  if(ang < LS && ang > LI){
+    cam2.write(ang);
+  }
+  delay(1000);
+  camdetach();
+}
+
+void cam2down(){
+  camatach();
+  int ang = cam2.read() - 10;
+  if(ang < LS && ang > LI){
+    cam2.write(ang);
+  }
+  delay(1000);
+  camdetach();
+}
+
+void ini(){
   motor1.write(ANG_M1);
   motor2.write(ANG_M2);
   motor3.write(ANG_M3);
@@ -149,75 +304,10 @@ void parar(){
   motor6.write(ANG_M6);
 }
 
-void izq(){
-  motor1.write(120);
-  motor3.write(120);
-  motor5.write(120);
-  
-  motor2.write(120);
-  motor4.write(120);
-  motor6.write(120);
-  delay(2000);
-  
-}
 
-void der(){
- motor1.write(0);
- motor3.write(0);
- motor5.write(0);
- 
- motor2.write(0);
- motor4.write(0);
- motor6.write(0);
- delay(2000);
-}
 
-void avanzar(){
-  adelante(motor1,0);
-  adelante(motor2,1);
-  adelante(motor3,0);
-  adelante(motor4,1);
-  adelante(motor5,0);
-  adelante(motor6,1);
-}
 
-void retroceder(){
-  atras(motor1,0);
-  atras(motor3,0);
-  atras(motor5,0);
-  atras(motor2,1);
-  atras(motor4,1);
-  atras(motor6,1);
-}
 
-void cam1izq(){
-  int ang = cam1.read() - 10;
-  if(ang < LS && ang > LI){
-    cam1.write(ang);
-  }
-}
-
-void cam1der(){
-  int ang = cam1.read() + 10;
-  if(ang < LS && ang > LI){
-    cam1.write(ang);
-  }
-}
-
-void cam2up(){
-  int ang = cam2.read() + 10;
-  if(ang < LS && ang > LI){
-    cam2.write(ang);
-  }
-
-}
-
-void cam2down(){
-  int ang = cam2.read() - 10;
-  if(ang < LS && ang > LI){
-    cam2.write(ang);
-  }
-}
 
 
 

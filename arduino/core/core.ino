f26ef40ac4993ccdb7ff552a5cd62146913d1f27
sass3r <mrsblast@gmail.com>
2014-07-30 11:50:04 -0400

    updated firmware core.ino

--- arduino/core/core.ino
+++ arduino/core/core.ino
@@ -1,4 +1,9 @@
 #include <Servo.h>
+#include "DHT11.h"
+#include <string.h>
+
+#define DHTPIN 3
+#define DHTTYPE DHT11
 
 #define M1 8
 #define M2 9
@@ -8,6 +13,8 @@
 #define M6 13
 #define M7 4
 #define M8 5
+#define M9 6
+#define M10 7
 
 
 #define ANG_M1 87
@@ -20,9 +27,17 @@
 
 #define ANG_M7 120
 #define ANG_M8 90
+
 #define LI 0
 #define LS 180
 
+#define TEMP_CALC '\xee'
+#define CHANGE_MODE '\xef'
+#define MEDIR_DIST '\xf0'
+#define IR_RIGHT '\xf1'
+#define IR_LEFT '\xf2'
+#define MIC_UP '\xf3'
+#define MIC_DOWN '\xf4'
 #define CAM1_RIGHT_FWD '\xf5'
 #define CAM1_LEFT_BWD '\xf6'
 #define CAM1_LEFT '\xf7' 
@@ -35,21 +50,34 @@
 #define DERECHA '\xfe'
 #define PARAR '\xff'
 
-Servo motor1,motor2,motor3,motor4,motor5,motor6, cam1, cam2;
+Servo motor1,motor2,motor3,motor4,motor5,motor6, cam1, cam2, mic, ir;
 char cmd;
-
+int ir_sensor = A0;
+DHT11 dht(DHTPIN);
 void setup(){
-  
+  pinMode(DHTPIN, INPUT);
   atachar();
   ini();
   detachar();
   
-  
+
   camatach();
   cam1.write(ANG_M7);
   cam2.write(ANG_M8);
   delay(1000);
   camdetach();
+  
+  mic.attach(M10);
+  mic.write(80);
+  delay(1000);
+  mic.detach();
+  
+  ir.attach(M9);
+  ir.write(70);
+  delay(1000);
+  ir.detach();
+  
+  
   Serial.begin(9600);
  
 }
@@ -58,48 +86,79 @@ void loop(){
   if(Serial.available() > 0){
     cmd = (char) Serial.read();
     if(cmd == ADELANTE){
-      Serial.println("adelante");
+      //Serial.println("adelante");
       avanzar();      
     }else{
       if(cmd == RETROCEDER){
-        Serial.println("retroceder");
+        //Serial.println("retroceder");
         retroceder();
       }else{
         if(cmd == IZQUIERDA){
-          Serial.println("izquierda");
+          //Serial.println("izquierda");
           izq();
         }else{
           if(cmd == DERECHA){
-            Serial.println("derecha");
+            //Serial.println("derecha");
             der();
           }else{
             if(cmd == PARAR){
-              Serial.println("parar");
+              //Serial.println("parar");
               parar();
             }else{
               if(cmd == CAM1_LEFT){
-                Serial.println("moviendo camara hacia la izq");
+                //Serial.println("moviendo camara hacia la izq");
                 cam1izq();
               }else{
                 if(cmd == CAM1_RIGHT){
-                  Serial.println("moviendo camara hacia la der");
+                  //Serial.println("moviendo camara hacia la der");
                   cam1der();
                 }else{
                   if(cmd == CAM2_UP){
-                    Serial.println("moviendo camara hacia arriba");
+                    //Serial.println("moviendo camara hacia arriba");
                     cam2up();
                   }else{
                     if(cmd == CAM2_DOWN){
-                      Serial.println("moviendo camara hacia abajo");
+                      //Serial.println("moviendo camara hacia abajo");
                       cam2down();                    
                     }else{
                       if(cmd == CAM1_LEFT_BWD){
-                        Serial.println("moviendo camara hacia la izq BWD");
+                        //Serial.println("moviendo camara hacia la izq BWD");
                         cam1izqbwd();
                       }else{
                         if(cmd == CAM1_RIGHT_FWD){
-                          Serial.println("moviendo camara hacia la der FWD");
+                          //Serial.println("moviendo camara hacia la der FWD");
                           cam1derfwd();
+                        }else{
+                          if(cmd == MIC_DOWN){
+                            //Serial.println("moviendo microscopio hacia abajo");
+                            micdown();
+                          }else{
+                            if(cmd == MIC_UP){
+                              //Serial.println("moviendo microscopio hacia arriba");
+                              micup();
+                            }else{
+                              if(cmd == IR_LEFT){
+                                irder();
+                              }else{
+                                if(cmd == IR_RIGHT){
+                                  irizq();
+                                }else{
+                                  if(cmd == MEDIR_DIST ){
+                                    medir();
+                                  }else{
+                                    if(cmd == CHANGE_MODE){
+                                      //Serial.println("cambiando de modo");
+                                      changemode();
+                                    }else{
+                                      if(cmd == TEMP_CALC){
+                                        readtemph();
+                                      }
+                                    }
+                                  }
+                                }
+                              }
+                            }
+                          }                     
                         }
                       }
                     }
@@ -295,6 +354,28 @@ void cam2down(){
   camdetach();
 }
 
+void micup(){
+  mic.attach(M10);
+  Serial.println(Serial.read());
+  for(int i = 110; i > 90; i--){
+    mic.write(mic.read()-5);
+    delay(70);
+  }
+  Serial.println(mic.read());
+  mic.detach();
+}
+
+void micdown(){
+  mic.attach(M10);
+  Serial.println(Serial.read());
+  for(int i = 90; i < 110; i++){
+    mic.write(mic.read() + 5);
+    delay(70);
+  }
+  Serial.println(mic.read());
+  mic.detach();
+}
+
 void ini(){
   motor1.write(ANG_M1);
   motor2.write(ANG_M2);
@@ -304,12 +385,65 @@ void ini(){
   motor6.write(ANG_M6);
 }
 
+void irder(){
+  ir.attach(M9);
+  Serial.println(ir.read());
+  if(ir.read() == 70){
+    for(int i = 0; i < 14; i++){
+    ir.write(ir.read() - 5);
+    delay(70);
+    }
+  }else{
+    for(int i = 0; i < 22; i++){
+    ir.write(ir.read() - 5);
+    delay(70);
+    }
+  }
+  ir.detach();
+}
 
+void irizq(){
+  ir.attach(M9);
+  Serial.println(ir.read());
+  if(ir.read() == 70){
+    for(int i = 0; i < 22 ; i++){
+    ir.write(ir.read() + 5);
+    delay(70);
+    }
+  }else{
+    //if(ir.read() == 0){
+      for(int i = 0; i < 14; i++){
+        ir.write(ir.read() + 5);
+        delay(70);
+      }
+    //}
+  }
+  ir.detach();
+}
 
+void readtemph(){
+  float temp, humedad;
+  dht.read(humedad,temp);
+  delay(100);
+  String puntos = ":";
+  char tmp[10];
+  char hm[10];
+  dtostrf(temp,1,2,tmp);
+  dtostrf(humedad,1,2,hm);
+  String lectura = tmp + puntos;
+  String medicion = lectura + hm; 
+  Serial.println(medicion);
+  
+}
 
+void medir(){
+  int lectura, cm;
+  lectura = analogRead(ir_sensor); //lectura del sensor ir
+  cm = pow(3027.4/lectura, 1.2134); //conversion a centimetros
+  Serial.println(cm);
+  delay(100);
+}
 
+void changemode(){
 
-
-
-
-
+}
